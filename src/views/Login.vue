d15048dc88a4827b30028d327da8244043e11c47
yuluohuangquan <648779925@qq.com>
2022-02-24 22:55:33 +0800

    less引入和login初始化

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <div class="login-content">
+      <el-form ref="formRef" :model="loginForm" label-width="80px">
+        <el-form-item label="名称">
+          <el-input v-model="loginForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            show-password
+          />
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" @click="submit()">登录</el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+export default {
+  name: "Login",
+  setup() {
+    const loginForm = reactive({
+      name: "",
+      password: "",
+    });
+
+    const router = useRouter();
+
+    const submit = () => {
+      console.log(loginForm);
+      router.push('/');
+    };
+
+    return { loginForm, submit };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background-image: url("../assets/login_bg.png");
+  background-size: 100%;
+  .login-content {
+      background: aliceblue;
+    position: absolute;
+    width: 320px;
+    height: 140px;
+    top: 200px;
+    margin: 0 auto;
+    text-align: center;
+    padding: 24px;
+    border-radius: 4px;
+    left: 50%;
+    margin-left: -160px;
+  }
+}
+</style>
